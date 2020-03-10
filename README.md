c757ea3fb9c042f0f695beab8b9ae59df534b738
Joaquim Castilla <joaquim@axelspace.com>
2020-03-10 22:58:50 +0900

    Added installation of oh-my-zsh. Requirements added to readme.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dotfiles
+
+## Requirements
+- zsh
+- curl
+- git
--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-DOT_FILES=( .vimrc .zshrc .zshrc.alias .zshrc.utils )
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Link dotfiles
+DOT_FILES=( .vimrc .zshrc .zshrc.alias .zshrc.utils )
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
@@ -8,6 +11,7 @@ done
 # Install FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
 # Install VundleVim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
