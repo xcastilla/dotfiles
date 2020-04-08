2820417e2cf3a59534a113f16a3de51ba89f6519
Joaquim Castilla <xcastilla89@gmail.com>
2020-04-08 22:13:17 +0900

    Added autosuggestion and syntax highlighting plugins to zsh config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # Install FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
